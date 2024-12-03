52763818013076d814bd919b90626e9d9a6873f8
Kriztan Sumbillo <kriztanvalenzuela@gmail.com>
2024-12-03 21:11:10 +0800

    Add project files.

---
+++ DashBoard/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ActiveCaptionText
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Image = CType(resources.GetObject("Button1.Image"), Image)
+        Button1.Location = New Point(217, 395)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(30, 31)
+        Button1.TabIndex = 0
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ActiveCaptionText
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Image = CType(resources.GetObject("Button2.Image"), Image)
+        Button2.Location = New Point(304, 395)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(30, 31)
+        Button2.TabIndex = 1
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ActiveCaptionText
+        Button3.FlatAppearance.BorderSize = 0
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Image = CType(resources.GetObject("Button3.Image"), Image)
+        Button3.Location = New Point(392, 395)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(30, 33)
+        Button3.TabIndex = 2
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(227), CByte(227))
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(683, 468)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        DoubleBuffered = True
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ DashBoard/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Diagnostics
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim url As String = "https://www.instagram.com/next_gen.computer/?hl=en"
+        OpenUrl(url)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim url As String = "https://www.facebook.com/profile.php?id=100066757736115"
+        OpenUrl(url)
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim url As String = "https://x.com/nextgencomp_07"
+        OpenUrl(url)
+    End Sub
+
+    Private Sub OpenUrl(url As String)
+        Try
+            Dim psi As New ProcessStartInfo() With {
+                .FileName = url,
+                .UseShellExecute = True
+            }
+            Process.Start(psi)
+        Catch ex As Exception
+            MessageBox.Show($"Failed to open URL: {url}. Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
